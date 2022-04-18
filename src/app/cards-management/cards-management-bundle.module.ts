60fdd516ca2131519524d23c9634b3e142d0cc37
Mehrez Hrizi <mehrez.hrizi.official@gmail.com>
2022-04-18 14:28:15 +0100

    configure the journey

--- src/app/cards-management/cards-management-bundle.module.ts
+++ src/app/cards-management/cards-management-bundle.module.ts
@@ -2,12 +2,22 @@ import {NgModule} from "@angular/core";
 import {CardsManagementJourneyModule} from "@backbase/cards-management-journey-ang";
 import {TemplateRegistry} from "@backbase/foundation-ang/core";
 
+import { CardsManagementJourneyConfigurationToken, CardsManagementJourneyConfiguration } from "@backbase/cards-management-journey-ang";
+
 @NgModule({
 imports: [
   CardsManagementJourneyModule.forRoot()
 ],
   providers: [
     TemplateRegistry,
+    {
+      provide: CardsManagementJourneyConfigurationToken,
+      useValue: <Partial<CardsManagementJourneyConfiguration>> {
+        notificationTtl: 5000,
+        groupByPaymentCardTypes: '',
+        enableTravelNotice: false
+      }
+    }
   ]
 })
 export class CardsManagementBundleModule {
